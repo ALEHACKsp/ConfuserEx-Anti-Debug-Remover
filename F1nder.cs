168976d3e3f3e326c1b9a15be61a33f6f408cba8
KrawkReverser <36350571+KrawkReverser@users.noreply.github.com>
2018-10-12 19:03:37 -0300

    Basic ConfuserEx Anti-Debug Remover

---
+++ F1nder.cs
@@ -0,0 +1,35 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anti_Debug_Remover
+{
+    internal static class F1nder
+    {
+        internal static int Run(this MethodDef method, OpCode opCode, object operand)
+        {
+            var num = 0;
+            foreach (var instruction in method.Body.Instructions)
+            {
+                if (instruction.OpCode != opCode)
+                    continue;
+                if (operand is int)
+                {
+                    var value = instruction.GetLdcI4Value();
+                    if (value == (int)operand)
+                        num++;
+                }
+                else if (operand is string)
+                {
+                    var value = instruction.Operand.ToString();
+                    if (value.Contains(operand.ToString()))
+                        num++;
+                }
+            }
+            return num;
+        }
+
+    }
+}
